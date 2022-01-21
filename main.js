e34c1dc10fd2367ea26a37ab36f40f886c3fae8f
Tamir Hen <51208520+TamirHen@users.noreply.github.com>
2022-01-21 11:25:48 -0800

    Express server was created

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+require('dotenv').config()
+const express = require('express');
+const PORT = process.env.PORT || 4000;
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`)
+})
