173d8ad0dd6091ed0442df63e1de779750ccec9f
Tamir Hen <51208520+TamirHen@users.noreply.github.com>
2022-01-21 15:39:20 -0800

    CSV to json and request to API were created

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express');
+const axios = require('axios');
 const {Client} = require('pg');
+const csv=require('csvtojson')
 const {fetchData} = require('./fetchData');
 const port = process.env.PORT || 4000;
 
@@ -19,13 +21,7 @@ const db = new Client({
 
 app.listen(port, async () => {
     console.log(`App running on port ${port}.`)
-    console.log(`Connecting to database...`)
-    try {
-        await db.connect()
-        console.log('Success')
-    } catch (error) {
-        console.error('Failed to connect', error)
-    }
+
     const config = {
         host: process.env.SFTP_HOST,
         username: process.env.SFTP_USERNAME,
@@ -33,7 +29,30 @@ app.listen(port, async () => {
     }
     const remotePath = '/Interview Task - Categorisation.csv';
     const localPath = 'data/Categorisation.csv';
-
+    // fetch csv file from sftp server
     await fetchData(config, remotePath, localPath)
+
+    console.log(`Connecting to database...`)
+    try {
+        await db.connect()
+        console.log('Success')
+    } catch (error) {
+        console.error('Failed to connect', error)
+    }
+
+    // convert csv file to json
+    const data = await csv().fromFile(localPath)
+    for (const {URL: url, Category: category} of data) {
+        const requestParams = new URLSearchParams({
+            type: 'url_organic',
+            key: process.env.SEMrush_API_KEY,
+            display_limit: 10,
+            export_columns: 'Ph,Po',
+            url,
+            database: 'uk'
+        })
+        const response = await axios.get(`${process.env.SEMrush_API_URL}?${requestParams.toString()}`)
+        console.log(response)
+    }
 
 })
